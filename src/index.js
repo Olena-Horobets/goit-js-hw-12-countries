5ffaca1e4c6de33b60b91296b17017b520ff65e2
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-09-17 23:35:36 +0300

    country card markup generation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 import './sass/main.scss';
 import fetchCountries from './js/fetchCountries';
 import refs from './js/refs';
-import { debounce } from 'lodash';
+import { before, debounce } from 'lodash';
+import countryMarkupTpl from './templates/countryMarkup.hbs';
 
 refs.searchInput.addEventListener('input', debounce(onSearchInput, 500));
 
 function onSearchInput(e) {
   if (e.target.value === '') {
+    refs.countriesList.innerHTML = '';
     return;
   }
+
   fetchCountries(e.target.value)
     .then(list => {
-      return list
-        .map(el => {
-          return `<li>${el.name}</li>`;
-        })
-        .join('');
+      if (list.status > 400 && list.status <= 499) {
+        console.log('Enter valid country name');
+        refs.countriesList.innerHTML = '';
+        e.target.value = '';
+        return;
+      } else if (list.length > 10) {
+        console.log('Enter more specific name, please');
+      } else if (list.length > 1 && list.length < 10) {
+        let countriesListMarkup = list
+          .map(el => {
+            return `<li class="countries__item">${el.name}</li>`;
+          })
+          .join('');
+        refs.countriesList.innerHTML = countriesListMarkup;
+      } else if (list.length === 1) {
+        const markup = countryMarkupTpl(list);
+        console.log('render');
+        refs.countriesList.innerHTML = '';
+        refs.container.insertAdjacentHTML('beforeend', markup);
+
+        return;
+      }
     })
-    .then(list => {
-      refs.countriesList.innerHTML = list;
+    .catch(err => {
+      console.error(err);
     });
 }
